fe71915fee1e1221d7965f73164ab4e678c09473
Jeremy Daer <jeremy@hey.com>
2026-07-27 01:24:58 -0700

    Regenerate for nullable timeline bounds + EverythingBoost re-export from below

--- ruby/lib/basecamp/generated/types.rb
+++ ruby/lib/basecamp/generated/types.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 # Auto-generated from OpenAPI spec. Do not edit manually.
-# Generated: 2026-07-27T07:34:03Z
+# Generated: 2026-07-27T08:24:49Z
 
 require "json"
 require "time"
@@ -3829,7 +3829,7 @@ module Basecamp
           "all_day" => @all_day,
           "ends_at" => @ends_at,
           "starts_at" => @starts_at,
-        }.compact
+        }.reject { |k, v| v.nil? && !["ends_at", "starts_at"].include?(k) }
       end
 
       def to_json(*args)
